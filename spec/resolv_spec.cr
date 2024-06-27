9050cc66fb98a57b91b3b4366c051e296e27f424
Anton Maminov <anton.linux@gmail.com>
2024-06-27 13:50:00 +0300

    fix specs

--- spec/resolv_spec.cr
+++ spec/resolv_spec.cr
@@ -3,12 +3,12 @@ require "./spec_helper"
 describe Resolv do
   it "works" do
     dns = Resolv::DNS.new("8.8.8.8", 5.seconds)
-    a_records = dns.a_resources("crystal-lang.org")
+    a_records = dns.a_resources("shards.info")
 
     a_records.should be_a(Array(Resolv::DNS::Resource::A))
-    a_records.size.should eq(4)
+    a_records.size.should eq(1)
 
     addresses = a_records.map(&.address)
-    addresses.should contain("18.244.146.39")
+    addresses.should contain("67.205.136.192")
   end
 end
