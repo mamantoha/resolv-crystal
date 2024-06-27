318b5e4d531cded33faeb6c06c464f0da693a16f
Anton Maminov <anton.linux@gmail.com>
2024-06-27 13:40:37 +0300

    add specs

--- spec/resolv_spec.cr
+++ spec/resolv_spec.cr
@@ -1,9 +1,14 @@
 require "./spec_helper"
 
 describe Resolv do
-  # TODO: Write tests
-
   it "works" do
-    false.should eq(true)
+    dns = Resolv::DNS.new("8.8.8.8", 5.seconds)
+    a_records = dns.a_resources("crystal-lang.org")
+
+    a_records.should be_a(Array(Resolv::DNS::Resource::A))
+    a_records.size.should eq(4)
+
+    addresses = a_records.map(&.address)
+    addresses.should contain("18.244.146.39")
   end
 end
