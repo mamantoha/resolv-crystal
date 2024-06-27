f7dbbc0899e8430de3d2a3633222c233f078a344
Anton Maminov <anton.linux@gmail.com>
2024-06-27 18:25:59 +0300

    update specs

--- spec/resolv_spec.cr
+++ spec/resolv_spec.cr
@@ -1,14 +1,25 @@
 require "./spec_helper"
 
 describe Resolv do
-  it "works" do
+  it "A records" do
     dns = Resolv::DNS.new("8.8.8.8", 5.seconds, retry: 3)
-    a_records = dns.a_resources("shards.info")
+    records = dns.a_resources("shards.info")
 
-    a_records.should be_a(Array(Resolv::DNS::Resource::A))
-    a_records.size.should eq(1)
+    records.should be_a(Array(Resolv::DNS::Resource::A))
+    records.size.should eq(1)
 
-    addresses = a_records.map(&.address)
+    addresses = records.map(&.address)
     addresses.should contain("67.205.136.192")
+  end
+
+  it "AAAA records" do
+    dns = Resolv::DNS.new("8.8.8.8", 5.seconds, retry: 3)
+    records = dns.aaaa_resources("wikimedia.org")
+
+    records.should be_a(Array(Resolv::DNS::Resource::AAAA))
+    records.size.should eq(1)
+
+    addresses = records.map(&.address)
+    addresses.should contain("2a02:ec80:300:ed1a::1")
   end
 end
