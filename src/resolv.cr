993fb26d89d1f06a0334d0089f2b062f487ab576
Anton Maminov <anton.linux@gmail.com>
2024-06-29 12:03:31 +0300

    move RCode to DNS

--- src/resolv.cr
+++ src/resolv.cr
@@ -9,33 +9,6 @@ module Resolv
     end
   end
 
-  # DNS RCODEs
-  #
-  # See http://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6 for reference
-  enum RCode
-    NoError   =  0
-    FormErr   =  1
-    ServFail  =  2
-    NXDomain  =  3
-    NotImp    =  4
-    Refused   =  5
-    YXDomain  =  6
-    YXRRSet   =  7
-    NXRRSet   =  8
-    NotAuth   =  9
-    NotZone   = 10
-    DSOTYPENI = 11
-    BADVERS   = 16
-    BADSIG    = 16
-    BADKEY    = 17
-    BADTIME   = 18
-    BADMODE   = 19
-    BADNAME   = 20
-    BADALG    = 21
-    BADTRUNC  = 22
-    BADCOOKIE = 23
-  end
-
   class DNS
     # Default DNS Port
     PORT = 53
@@ -52,6 +25,33 @@ module Resolv
                       Array(Resource::TXT) |
                       Array(Resource::AAAA) |
                       Array(Resource::SRV)
+
+    # DNS RCODEs
+    #
+    # See http://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6 for reference
+    enum RCode
+      NoError   =  0
+      FormErr   =  1
+      ServFail  =  2
+      NXDomain  =  3
+      NotImp    =  4
+      Refused   =  5
+      YXDomain  =  6
+      YXRRSet   =  7
+      NXRRSet   =  8
+      NotAuth   =  9
+      NotZone   = 10
+      DSOTYPENI = 11
+      BADVERS   = 16
+      BADSIG    = 16
+      BADKEY    = 17
+      BADTIME   = 18
+      BADMODE   = 19
+      BADNAME   = 20
+      BADALG    = 21
+      BADTRUNC  = 22
+      BADCOOKIE = 23
+    end
 
     class Resource
       enum Type
