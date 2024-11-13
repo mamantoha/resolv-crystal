df65a53c5285990a7a39fd85bb28151b7747403f
Anton Maminov <anton.linux@gmail.com>
2024-11-13 15:19:06 +0200

    add macos example

---
+++ samples/resolv_macos.cr
@@ -0,0 +1,46 @@
+require "socket"
+
+class Socket
+  struct IPAddress
+    def self.from(sockaddr : LibC::SockaddrIn*) : IPAddress
+      new(sockaddr, sizeof(typeof(sockaddr)))
+    end
+  end
+end
+
+@[Link("resolv")]
+lib LibResolv
+  fun ninit = res_9_ninit(Pointer(State)) : Int32
+  fun nclose = res_9_nclose(Pointer(State)) : Void
+
+  struct State
+    nscount : Int32
+    nsaddr_list : StaticArray(LibC::SockaddrIn, 3)
+  end
+
+  # fun state = __res_state : Pointer(State)
+  fun state = _res : Pointer(State)
+end
+
+def get_dns_server_list : Array(String)
+  state_ptr = Pointer(LibResolv::State).malloc(sizeof(LibResolv::State))
+  LibResolv.ninit(state_ptr)
+  state = state_ptr.value
+  LibResolv.nclose(state_ptr)
+
+  p! state
+
+  dns_servers = [] of String
+
+  state.nsaddr_list.each do |addr|
+    next if addr.sin_port == 0
+    next if addr.sin_family == 0
+
+    ip_string = Socket::IPAddress.from(pointerof(addr)).address
+    dns_servers << ip_string
+  end
+
+  dns_servers
+end
+
+puts get_dns_server_list
