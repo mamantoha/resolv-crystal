1066ad2a8dc08bea8a3ff9fe835502e091a297cd
Anton Maminov <anton.linux@gmail.com>
2024-11-13 17:44:59 +0200

    more flags

--- src/ext/lib_c/darwin/lib_resolv.cr
+++ src/ext/lib_c/darwin/lib_resolv.cr
@@ -1,10 +1,12 @@
-@[Link("resolv")]
-lib LibResolv
-  fun ninit = res_9_ninit(Pointer(State)) : Int32
-  fun nclose = res_9_nclose(Pointer(State)) : Void
+{% if flag?(:darwin) %}
+  @[Link("resolv")]
+  lib LibResolv
+    fun ninit = res_9_ninit(Pointer(State)) : Int32
+    fun nclose = res_9_nclose(Pointer(State)) : Void
 
-  struct State
-    nscount : Int32
-    nsaddr_list : StaticArray(LibC::SockaddrIn, 3)
+    struct State
+      nscount : Int32
+      nsaddr_list : StaticArray(LibC::SockaddrIn, 3)
+    end
   end
-end
+{% end %}
--- src/ext/lib_c/linux/lib_resolv.cr
+++ src/ext/lib_c/linux/lib_resolv.cr
@@ -1,11 +1,13 @@
-@[Link("resolv")]
-lib LibResolv
-  fun init = __res_init : Int32
+{% if flag?(:linux) %}
+  @[Link("resolv")]
+  lib LibResolv
+    fun init = __res_init : Int32
 
-  struct State
-    nscount : Int32
-    nsaddr_list : StaticArray(LibC::SockaddrIn, 3)
+    struct State
+      nscount : Int32
+      nsaddr_list : StaticArray(LibC::SockaddrIn, 3)
+    end
+
+    fun state = __res_state : Pointer(State)
   end
-
-  fun state = __res_state : Pointer(State)
-end
+{% end %}
